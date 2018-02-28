5393cbaf01224b4040d253cfd282b41c84d2519b
Ross Markello <rossmarkello@gmail.com>
2018-02-28 17:14:15 -0500

    [DOC] Some minore docstring updates

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ import sys
 def main():
     from setuptools import setup, find_packages
 
+    if sys.version_info < (3, 5):
+        raise SystemError('You need Python version 3.5 or above to use ' +
+                          'this package. Current\nversion detected is ' +
+                          sys.version.split(' ')[0])
+
     # from nipype setup.py file
     ldict = locals()
     curr_path = os.path.dirname(__file__)
