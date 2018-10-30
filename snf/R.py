aa832cc062c4b3fb740ace389a3da81926cffd71
Ross Markello <rossmarkello@gmail.com>
2018-10-30 15:51:09 -0400

    [REF] Remove plotting submodule

--- snf/R.py
+++ snf/R.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from snf import compute, plotting, metrics
+from snf import compute, metrics
 from sklearn.cluster import spectral_clustering
 
 affinityMatrix = compute.affinity_matrix
 calNMI = metrics.normalized_mutual_info_score
 concordanceNetworkMNI = metrics.nmi
-displayClustersWithHeatmap = plotting.mod_heatmap
 dist2 = compute.dist2
 estimateNumberOfClustersGivenGraph = compute.get_n_clusters
 rankFeaturesByNMI = metrics.rank_feature_by_nmi
--- snf/matlab.py
+++ snf/matlab.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from snf import compute, plotting, metrics
+from snf import compute, metrics
 from sklearn.cluster import spectral_clustering
 
 affinityMatrix = compute.affinity_matrix
 B0normalized = compute._B0_normalized
 Cal_NMI = metrics.normalized_mutual_info_score
 Concordance_Network_NMI = metrics.nmi
-displayClusters = plotting.mod_heatmap
 dist2 = compute.dist2
 Estimate_Number_of_Clusters_given_graph = compute.get_n_clusters
 FindDominateSet = compute._find_dominate_set
