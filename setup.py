2b9b431e8aeab1509e070bb3b53fd9307592aae7
Ross Markello <rossmarkello@gmail.com>
2019-10-04 12:05:47 -0400

    [FIX] Overhaul installation
    To try and fix dumb testing bug...

--- setup.py
+++ setup.py
@@ -1,53 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import sys
 
+from setuptools import setup
+import versioneer
 
-def main():
-    import versioneer
-    from io import open
-    import os.path as op
-    from inspect import getfile, currentframe
-    from setuptools import setup, find_packages
+SETUP_REQUIRES = ['setuptools >= 30.3.0']
+SETUP_REQUIRES += ['wheel'] if 'bdist_wheel' in sys.argv else []
 
-    root_dir = op.dirname(op.abspath(getfile(currentframe())))
-
-    ldict = locals()
-    with open(op.join(root_dir, 'snf', 'info.py')) as infofile:
-        exec(infofile.read(), globals(), ldict)
-
-    # get long description from README
-    with open(op.join(root_dir, ldict['__longdesc__'])) as src:
-        ldict['__longdesc__'] = src.read()
-
-    ldict.setdefault('__version__', versioneer.get_version())
-    ldict.setdefault('__cmdclass__', versioneer.get_cmdclass())
-    DOWNLOAD_URL = (
-        'https://github.com/rmarkello/{name}/archive/{ver}.tar.gz'.format(
-            name=ldict['__packagename__'],
-            ver=ldict['__version__']))
-
-    setup(
-        name=ldict['__packagename__'],
-        version=ldict['__version__'],
-        description=ldict['__description__'],
-        long_description=ldict['__longdesc__'],
-        long_description_content_type=ldict['__longdesctype__'],
-        author=ldict['__author__'],
-        author_email=ldict['__email__'],
-        maintainer=ldict['__maintainer__'],
-        maintainer_email=ldict['__email__'],
-        url=ldict['__url__'],
-        license=ldict['__license__'],
-        classifiers=ldict['CLASSIFIERS'],
-        download_url=DOWNLOAD_URL,
-        install_requires=ldict['INSTALL_REQUIRES'],
-        packages=find_packages(exclude=['snf/tests']),
-        package_data=ldict['PACKAGE_DATA'],
-        tests_require=ldict['TESTS_REQUIRE'],
-        extras_require=ldict['EXTRAS_REQUIRE'],
-        cmdclass=ldict['__cmdclass__']
-    )
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    setup(name='snfpy',
+          setup_requires=SETUP_REQUIRES,
+          version=versioneer.get_version(),
+          cmdclass=versioneer.get_cmdclass())
--- snf/__init__.py
+++ snf/__init__.py
@@ -1,22 +1,10 @@
 __all__ = [
-    '__author__', '__description__', '__email__', '__license__',
-    '__maintainer__', '__packagename__', '__url__', '__version__',
-    'cv', 'metrics', 'make_affinity', 'snf', 'get_n_clusters'
+    '__version__', 'cv', 'metrics', 'make_affinity', 'snf', 'get_n_clusters'
 ]
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-from .info import (
-    __author__,
-    __description__,
-    __email__,
-    __license__,
-    __maintainer__,
-    __packagename__,
-    __url__,
-)
 
 from . import (cv, metrics)
 from .compute import (make_affinity, snf, get_n_clusters)
