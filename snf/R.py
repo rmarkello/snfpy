7d9eca409d1d84809d5e574b2577e6550ffafaea
Ross Markello <rossmarkello@gmail.com>
2019-10-04 11:28:55 -0400

    [FIX] Fixes nmi import error

--- snf/R.py
+++ snf/R.py
@@ -6,7 +6,7 @@ Aliases for easier compatibility with the SNF toolbox written in R
 from . import compute as _compute, metrics as _metrics
 
 affinityMatrix = _compute.affinity_matrix
-calNMI = _metrics.normalized_mutual_info_score
+calNMI = _metrics.v_measure_score
 concordanceNetworkMNI = _metrics.nmi
 estimateNumberOfClustersGivenGraph = _compute.get_n_clusters
 rankFeaturesByNMI = _metrics.rank_feature_by_nmi
--- snf/matlab.py
+++ snf/matlab.py
@@ -7,7 +7,7 @@ from . import compute as _compute, metrics as _metrics
 
 affinityMatrix = _compute.affinity_matrix
 B0normalized = _compute._B0_normalized
-Cal_NMI = _metrics.normalized_mutual_info_score
+Cal_NMI = _metrics.v_measure_score
 Concordance_Network_NMI = _metrics.nmi
 Estimate_Number_of_Clusters_given_graph = _compute.get_n_clusters
 FindDominateSet = _compute._find_dominate_set
